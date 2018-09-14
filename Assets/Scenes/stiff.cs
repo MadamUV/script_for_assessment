6a398fbf55683ec77b7b77fd6859dc1b933c5d9b
Erin M Masterson <erin.margot.masterson@gmail.com>
2018-09-13 23:44:27 -0400

    attempted to use bounds to prevent edge distortion upon cloth colliding with sphere

---
+++ Assets/Scenes/stiff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stiff : MonoBehaviour {
+    public GameObject myGameObject;
+    private Mesh myMesh;
+    private Cloth myCloth;
+    private Bounds myBounds;
+    private Vector3 min;
+    private Vector3 max;
+	// Use this for initialization
+	void Start () {
+        myMesh = myGameObject.GetComponent<MeshFilter>().mesh;
+        myCloth = myGameObject.GetComponent<Cloth>();
+        myBounds = myMesh.bounds;
+        min = myBounds.min;
+        max = myBounds.max;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
